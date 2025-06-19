ddb8b29fed67ea2366d365bf52963cb7985dff02
Lucas Tjor <tjor75@gmail.com>
2025-06-19 11:55:15 -0300

    Configurar provisionalmente layout y navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Layout from './pages/Layout';
 import HomePage from './pages/HomePage';
 import PersonaPage from './pages/PersonaPage';
 import EstadisticasPage from './pages/EstadisticasPage';
 import ContactoPage from './pages/ContactoPage';
 import NoEncontradoPage from './pages/NoEncontradoPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/persona/:id" element={<PersonaPage />} />
           <Route path="/estadisticas" element={<EstadisticasPage />} />
