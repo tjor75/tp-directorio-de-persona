bcbba4bc1449bf00096c834d757d1141a01bd58f
Lucas Tjor <tjor75@gmail.com>
2025-06-19 12:15:53 -0300

    Solucionar error sin navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/estadisticas">Estadisticas</NavLink>
-        <NavLink to="/contacto">Contacto</NavLink>
-    </nav>
+    return (
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/estadisticas">Estadisticas</NavLink>
+            <NavLink to="/contacto">Contacto</NavLink>
+        </nav>
+    );
 }
 
 export default Navbar;
